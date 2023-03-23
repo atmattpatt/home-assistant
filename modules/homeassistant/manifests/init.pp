fb157ae8d86b47aeaf479cde84a7c8014001e699
Matthew Patterson <matthew.s.patterson@gmail.com>
2023-03-22 20:33:23 -0500

    Light renaming and reorganization

--- modules/homeassistant/manifests/init.pp
+++ modules/homeassistant/manifests/init.pp
@@ -1,4 +1,4 @@
-file { '/home-assistant/config/configuration.yaml':
+file { '/homeassistant/config/configuration.yaml':
   ensure => file,
   source => 'puppet:///modules/hass/configuration.yaml'
 }
