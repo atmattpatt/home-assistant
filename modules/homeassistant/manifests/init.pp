293ce329332920960ded4e1d933b2bb82694e5b0
Matthew Patterson <matthew.s.patterson@gmail.com>
2023-03-22 21:22:54 -0500

    Remove service from puppet

--- modules/homeassistant/manifests/init.pp
+++ modules/homeassistant/manifests/init.pp
@@ -1,13 +1,6 @@
 class homeassistant {
-  service { 'home-assistant':
-    hasstatus  => false,
-    hasrestart => true,
-    restart    => 'docker restart home-assistant',
-  }
-
   file { '/homeassistant/config/configuration.yaml':
     ensure => file,
     source => 'puppet:///modules/homeassistant/configuration.yaml',
-    notify => Service['home-assistant'],
   }
 }
