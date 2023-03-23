d30bdbf9fdbe3324d8581886e70cceccf6a5efb9
Matthew Patterson <matthew.s.patterson@gmail.com>
2023-03-22 21:06:20 -0500

    Try restarting

--- modules/homeassistant/manifests/init.pp
+++ modules/homeassistant/manifests/init.pp
@@ -1,6 +1,13 @@
 class homeassistant {
+  service { 'home-assistant':
+    hasstatus  => false,
+    hasrestart => true,
+    restart    => 'docker restart home-assistant',
+  }
+
   file { '/homeassistant/config/configuration.yaml':
     ensure => file,
-    source => 'puppet:///modules/homeassistant/configuration.yaml'
+    source => 'puppet:///modules/homeassistant/configuration.yaml',
+    notify => Service['home-assistant'],
   }
 }
